8a838855b508656378c4b029cd6e33ca57fc3104
piotr <PiotrBosak@yahoo.com>
2020-05-30 19:06:23 +0200

    added Beer class also fixec data-jpa problem(we have to specify the database used for it, we can just add h2 database dependency) closes #5

--- beer-service/src/main/java/pb/spring/BeerServiceApplication.java
+++ beer-service/src/main/java/pb/spring/BeerServiceApplication.java
@@ -1,9 +1,10 @@
-package pb.spring.beerservice;
+package pb.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class BeerServiceApplication {
 
 	public static void main(String[] args) {
