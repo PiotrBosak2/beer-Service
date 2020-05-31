ed6a2eb6da021f5fb661182693193665be7d3840
piotr <PiotrBosak@yahoo.com>
2020-05-31 10:19:10 +0200

    added validation dependency to pom.xml added validation to BeerDto closes #6

--- beer-service/src/main/java/pb/spring/web/model/BeerDto.java
+++ beer-service/src/main/java/pb/spring/web/model/BeerDto.java
@@ -5,23 +5,39 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+
+    @Null
     private UUID id;
+    @Null
     private Integer version;
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
     private String beerName;
+    @NotNull
     private BeerStyle beerStyle;
+    @NotNull
+    @Positive
     private Long upc;
+
+    @NotNull
+    @PositiveOrZero
     private BigDecimal price;
+
     private Integer quantityOnHand;
 
 }
