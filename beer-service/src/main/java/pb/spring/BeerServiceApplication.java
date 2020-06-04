bd05e928faae027a6aeb2c5917721476ac8bedc2
piotr <PiotrBosak@yahoo.com>
2020-06-04 10:01:52 +0200

    changed application.properties so that mysql works properly, specified varchar length in beer class

--- beer-service/src/main/java/pb/spring/BeerServiceApplication.java
+++ beer-service/src/main/java/pb/spring/BeerServiceApplication.java
@@ -2,9 +2,10 @@ package pb.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class BeerServiceApplication {
 
 	public static void main(String[] args) {
